4f130e86cd60f6d41133ddde372e909c8320b59d
Cade <ateam243@gmail.com>
2020-10-01 05:02:43 -0400

    Changed look of buttons and added a form.

--- main2.py
+++ main2.py
@@ -2,5 +2,5 @@ import webview
 import time
 
 if __name__ == '__main__':
-    webview.create_window('Capstone 1', 'http://127.0.0.1:8050/', resizable=False, background_color='#000')
+    webview.create_window('Capstone 1', 'http://127.0.0.1:8000/', resizable=True, background_color='#000')
     webview.start(http_server=True)
