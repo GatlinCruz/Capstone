1c77137a1a813d4e0cd2484a7a159db4d2bc2f4d
Gatlin Cruz <gatlin.cruz@gmail.com>
2020-11-09 18:51:23 -0500

    different stuff

--- mysite/gui/urls.py
+++ mysite/gui/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='gui-home'),
-    path('figure.html', views.graph, name='gui-graph')
+    path('figure.html', views.graph, name='gui-graph'),
 ]
--- mysite/gui/views.py
+++ mysite/gui/views.py
@@ -2,6 +2,8 @@ import importlib.util
 
 from django.shortcuts import render
 from pathlib import Path
+from django.template import Context, loader
+import webview
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -14,6 +16,17 @@ spec = importlib.util.spec_from_file_location("buttons", str(BASE_DIR) + "/gui/t
 buttons = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(buttons)
 
+
+page = open("/Users/gatlincruz/PycharmProjects/Capstone/mysite/gui/templates/gui/figure.html", "r").read()
+
+
+num_links = {
+        'num_hosts': 0,
+        'num_switches': 0,
+        'num_controllers': 0,
+        'links': []
+        }
+
 graph_info = {
         'num_hosts': 0,
         'num_switches': 0,
@@ -22,9 +35,12 @@ graph_info = {
         }
 
 context = {
-        'graph': graph_info
+        'graph': graph_info,
+        'first_name': page,
+        'num_hosts': num_links['num_hosts'],
+        'num_switches': num_links['num_switches'],
+        'num_controllers': num_links['num_controllers']
     }
-
 
 def home(request):
     if request.GET.get('setbtn'):
@@ -42,8 +58,16 @@ def home(request):
         graph_info['links'] = links
 
     elif request.GET.get('graphbtn'):
-        buttons.make_graph(graph_info['num_hosts'], graph_info['num_switches'],
-                           graph_info['num_controllers'], graph_info['links'])
+        #buttons.make_graph(graph_info['num_hosts'], graph_info['num_switches'],
+        #                   graph_info['num_controllers'], graph_info['links'])
+        #return render(request, 'gui/figure.html', context)
+        buttons.make_graph(num_links['num_hosts'], num_links['num_switches'],
+                            num_links['num_controllers'], num_links['links'])
+
+        #webview.create_window('Capstone 1', 'gui/figure.html', resizable=True, background_color='#000')
+        #webview.start(http_server=True)
+        reload()
+
         return render(request, 'gui/figure.html', context)
 
     elif request.GET.get('resetbtn'):
@@ -52,8 +76,35 @@ def home(request):
     elif request.GET.get('runbtn'):
         buttons.make_file(graph_info)
 
-    return render(request, 'gui/base.html', context)
+    elif request.POST.get('applybtn'):
+
+        numHosts = request.POST.get('num_hosts')
+        numSwitches = request.POST.get('num_switches')
+        numControllers = request.POST.get('num_controllers')
+        num_links['num_hosts'] = numHosts
+        num_links['num_switches'] = numSwitches
+        num_links['num_controllers'] = numControllers
+
+
+    elif request.POST.get('addbtn'):
+
+        link1 = request.POST.get('link1')
+        link2 = request.POST.get('link2')
+        num_links['links'].append((link1, link2))
+
+    elif request.POST.get('printbtn'):
+        print(num_links['num_hosts'])
+        print(num_links['num_switches'])
+        print(num_links['num_controllers'])
+        print(num_links['links'])
+
+    return render(request, 'gui/base1.html', context)
 
 
 def graph(request):
     return render(request, 'gui/figure.html')
+
+def reload():
+    page = open("/Users/gatlincruz/PycharmProjects/Capstone/mysite/gui/templates/gui/figure.html", "r").read()
+
+
