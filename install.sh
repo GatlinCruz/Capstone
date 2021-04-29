6d2e03bfcbd3b889d8a926abf95ef28e2f480276
cadet243 <ateam243@gmail.com>
2021-04-29 01:31:49 -0400

    Might work

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# script to install necessary dependencies
+
 # installing mininet
 sudo apt-get -y install mininet
 
@@ -22,16 +25,14 @@ sudo cp apoc.conf /etc/neo4j/
 sudo apt-get -y install python3-venv
 
 # installing OpenFlow switch, controller, and Wireshark
-cd ../
 git clone git://github.com/mininet/mininet
 mininet/util/install.sh -fw
-cd Capstone/
 
 # create venv
 python3 -m venv venv
 
 # activate venv
-. ~/Capstone/venv/bin/activate
+. ./venv/bin/activate
 
 # install dependencies
 pip install -r requirements.in
