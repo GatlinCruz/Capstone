0529717291307428faf80ee01d8ae51f2adc60a0
GatlinCruz <gatlin.cruz@yahoo.com>
2021-05-12 17:03:44 -0400

    Update db_testing.py
    Removed unneeded code

--- mysite/gui/templates/db_testing.py
+++ mysite/gui/templates/db_testing.py
@@ -23,74 +23,6 @@ class App:
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
-    def create_friendship(self, person1_name, person2_name):
-        """
-        Calls the static method _create_and_return_friendship to add nodes and their link
-        :param person1_name: The first node
-        :param person2_name: The second node
-        :return: None
-        """
-        with self.driver.session() as session:
-            # Write transactions allow the driver to handle retries and transient errors
-            result = session.write_transaction(
-                self._create_and_return_friendship, person1_name, person2_name)
-            for row in result:
-                print("Created friendship between: {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
-
-    @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
-        """
-        This method creates two nodes in the database and a link between them
-        :param tx: The tx object used to run the command
-        :param person1_name: The first node
-        :param person2_name: The second node
-        :return: The result from the call
-        """
-        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-        query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:O]->(p2) "
-            "RETURN p1, p2"
-        )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
-        try:
-            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]}
-                    for row in result]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
-
-    def find_person(self, person_name):
-        """
-        Calls the static method _find_and_return_person to find a node
-        :param person_name: The node that is being searched for
-        :return: None
-        """
-        with self.driver.session() as session:
-            result = session.read_transaction(self._find_and_return_person, person_name)
-            for row in result:
-                print("Found person: {row}".format(row=row))
-
-    @staticmethod
-    def _find_and_return_person(tx, person_name):
-        """
-        Finds a node in the database if it exists
-        :param tx: The tx object used to run the command
-        :param person_name: The node that is being searched for
-        :return: The node if it exists
-        """
-        query = (
-            "MATCH (p:Person) "
-            "WHERE p.name = $person_name "
-            "RETURN p.name AS name"
-        )
-        result = tx.run(query, person_name=person_name)
-        return [row["name"] for row in result]
-
     @staticmethod
     def _create_and_return_node(tx, node, graph_name, node_type, ip=None):
         """
@@ -164,18 +96,7 @@ class App:
         path = str(Path.home()) + "/Desktop/" + str(filename) + ".csv"
         return tx.run("CALL apoc.export.csv.all($path, {})", path=path).single()
 
-    # @staticmethod
-    # def test(tx):
-    #     query = ("MATCH (n:example)"
-    #              "RETURN n LIMIT 25"
-    #              )
-    #     return tx.run(query)
-    #
-    # def test1(self):
-    #     with self.driver.session() as session:
-    #         return session.read_transaction(self.test)
-
-
+""" Used to test """
 if __name__ == "__main__":
     # Aura queries use an encrypted connection using the "neo4j+s" URI scheme
     bolt_url = "neo4j://localhost:7687"  # %%BOLT_URL_PLACEHOLDER%%
